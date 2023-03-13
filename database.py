eafa2552c37c1467d50ceca114a39569beba6dac
SergeyPriem <s.priemshiy@gmail.com>
2023-03-13 12:15:27 +0500

    Updated Streamlit Version to 1.20.0 and Config

--- database.py
+++ database.py
@@ -4,22 +4,22 @@ from sqlmodel import create_engine
 import streamlit as st
 
 # def get_engine(sqlite_file):
-#     sleep(0.75)
 #     sqlite_url = f"sqlite:///{sqlite_file}"
 #     return create_engine(sqlite_url, echo=True)
 #
 #
 # sqlite_file_name = "database.db"
-#
 # engine = get_engine(sqlite_file_name)
 
 ###############################################################################################################
 
 
 engine = create_engine(st.secrets['DATABASE_URL'],
-                       pool_size=5,
-                       pool_recycle=3600,
+                       pool_size=10,
+                       pool_recycle=-1,
                        pool_timeout=30,
                        max_overflow=10,
-                       echo_pool=False,
-                       poolclass=QueuePool)
+                       echo_pool=True,
+                       poolclass=QueuePool,
+                       pool_pre_ping=True,
+                       )
