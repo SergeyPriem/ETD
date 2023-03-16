d1ee052e43e4d2f7f16b5b4b24f291e411e4f809
SergeyPriem <s.priemshiy@gmail.com>
2023-03-16 18:36:10 +0500

    Changed logo 2023-03-16

--- transmittals_tab.py
+++ transmittals_tab.py
@@ -2,7 +2,8 @@
 
 from time import sleep
 import streamlit as st
-
+import tkinter as tk
+from tkinter import filedialog
 
 def transmittals_content():
     tr_empty1, tr_content, tr_empty2 = st.columns([1,9,1])
@@ -19,14 +20,31 @@ def transmittals_content():
         sleep(1)
         # Replace the placeholder with some text:
         placeholder.text("Hello")
-        sleep(5)
+        sleep(1)
         # Replace the text with a chart:
         placeholder.line_chart({"data": [1, 5, 2, 6]})
-        sleep(5)
+        sleep(1)
         # Replace the chart with several elements:
         with placeholder.container():
             st.write("This is one element")
             st.write("This is another")
-        sleep(3)
+        sleep(1)
         # Clear all those elements:
         placeholder.empty()
+
+# import libraries
+
+
+# Set up tkinter
+root = tk.Tk()
+root.withdraw()
+
+# Make folder picker dialog appear on top of other windows
+root.wm_attributes('-topmost', 1)
+
+# Folder picker button
+st.title('Folder Picker')
+st.write('Please select a folder:')
+clicked = st.button('Folder Picker')
+if clicked:
+    dirname = st.text_input('Selected folder:', filedialog.askdirectory(master=root))
