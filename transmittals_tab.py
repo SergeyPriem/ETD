639ef81356081b66e712c20127965b8c8a6c0b79
Sergio <97326127+SergeyPriem@users.noreply.github.com>
2023-03-16 21:20:33 +0500

    Update transmittals_tab.py
    Transmittals

--- transmittals_tab.py
+++ transmittals_tab.py
@@ -36,15 +36,15 @@ def transmittals_content():
 
 
         # Set up tkinter
-        root = tk.Tk()
-        root.withdraw()
+        #root = tk.Tk()
+        #root.withdraw()
 
         # Make folder picker dialog appear on top of other windows
-        root.wm_attributes('-topmost', 1)
+        #root.wm_attributes('-topmost', 1)
 
         # Folder picker button
-        st.title('Folder Picker')
-        st.write('Please select a folder:')
-        clicked = st.button('Folder Picker')
-        if clicked:
-            dirname = st.text_input('Selected folder:', filedialog.askdirectory(master=tk.Tk())) #master=root
+        #st.title('Folder Picker')
+        #st.write('Please select a folder:')
+        #clicked = st.button('Folder Picker')
+        #if clicked:
+        #    dirname = st.text_input('Selected folder:', filedialog.askdirectory()) #master=root
