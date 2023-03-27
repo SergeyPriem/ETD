51ab84f1fd8df22c9ecab656d795edbdae88191d
SergeyPriem <s.priemshiy@gmail.com>
2023-03-27 14:51:29 +0500

    Fixing 2023-03-27

--- app.py
+++ app.py
@@ -284,23 +284,24 @@ def home_content():
                         st.session_state.upd_code_sent = True
 
                     entered_upd_code = st.text_input("Confirmation Code from Email")
-
+                    st.write("0")
                     if st.button("Confirm Code for Update"):
                         # if company_email in registered_emails:
                         #     reporter(f'User {company_email} is already in DataBase')
                         #     st.stop()
-                        # st.write(1)
+                        st.write(1)
                         st.write(st.session_state.user, upd_pass_2)
 
                         if st.session_state.upd_conf_num != entered_upd_code:
                             reporter("Confirmation code is wrong, try again")
-                            # st.write(2)
+                            st.write(2)
                             st.stop()
                         else:
-                            # st.write(3)
+                            st.write(3)
                             st.write(st.session_state.user, upd_pass_2)
                             reply = update_user_reg_data(st.session_state.user, upd_pass_2)
                             reporter(reply)
+                    st.write(4)
 
 
 st.cache_data(ttl=600)
