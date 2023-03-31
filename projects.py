ebb9b3dae9a297be4fa5dbbe079a403cd666f6d1
SergeyPriem <s.priemshiy@gmail.com>
2023-03-31 12:53:04 +0500

    Hiding tasks 2023-03-31

--- projects.py
+++ projects.py
@@ -435,11 +435,11 @@ def confirm_task(task_id, user, proj, sod):
             st.write(heroes)
             if user == heroes[0].id:
                 Task[task_id].coord_log = str(
-                    Task[task_id].coord_log.replace('None', '')) + f"*{user}*{datetime.now()}*\n"
+                    Task[task_id].coord_log).replace('None', '') + f"*{user}*{datetime.now()}*\n"
 
             if user == heroes[1].id:
                 Task[task_id].perf_log = str(
-                    Task[task_id].coord_log.replace('None', '')) + f"*{user}*{datetime.now()}*\n"
+                    Task[task_id].coord_log).replace('None', '') + f"*{user}*{datetime.now()}*\n"
 
         except Exception as e:
             st.warning(f"🔧 {type(e).__name__} {getattr(e, 'args', None)}")
