07ba84d4f01114348230fa5f88af6eb05267ec60
SergeyPriem <s.priemshiy@gmail.com>
2023-04-07 09:56:30 +0500

    Update 2023-04-07

--- projects.py
+++ projects.py
@@ -262,8 +262,8 @@ def create_sod(proj_short: str, set_name: str, stage: str, status: str, set_star
                 project_id=Project.get(short_name=proj_short),
                 set_name=set_name,
                 stage=stage,
-                coord_id=Users.get(login=coordinator),
-                perf_id=Users.get(login=performer),
+                coord_id=Users.get(login=coordinator).id,
+                perf_id=Users.get(login=performer).id,
                 current_status=status,
                 start_date=set_start_date,
                 notes=notes
