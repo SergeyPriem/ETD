f4ba2675e1bde3c581dd2f9508cdbda5049e645c
SergeyPriem <s.priemshiy@gmail.com>
2023-04-08 11:37:00 +0500

    Update 2023-04-08

--- drawing_sets_tab.py
+++ drawing_sets_tab.py
@@ -39,8 +39,9 @@ def drawing_sets():
         st.title(':orange[Drawing Sets]')
 
 
-        ds_left, ds_rigth = st.columns(2)
-        my_all = ds_rigth.radio("Select the Option", ["My Units", 'All Units'], horizontal=True)
+        ds_left, ds_center, ds_rigth = st.columns(3)
+        my_all = ds_center.radio("Select the Option", ["My Units", 'All Units'],
+                                 horizontal=True, label_visibility='collapsed')
         ds_left.subheader(my_all)
 
         if my_all == "My Units":
