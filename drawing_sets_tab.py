d23203e3116a421dcb3723d2b599ee12c06bb481
SergeyPriem <s.priemshiy@gmail.com>
2023-04-10 16:51:33 +0500

    Update 2023-04-10

--- drawing_sets_tab.py
+++ drawing_sets_tab.py
@@ -101,7 +101,7 @@ def drawing_sets():
 
             st.write(set_id) ###
 
-            units_tasks = get_own_tasks(int(set_id)) #.values[0]
+            units_tasks = get_own_tasks(int(set_id.values[0])) #.values[0]
 
             # if units_tasks == "Empty Table":
             #     st.warning('No Tasks Available for selected Unit')
