27f20b741cd7f154b4e88354b9543373c67dd09d
SergeyPriem <s.priemshiy@gmail.com>
2023-04-13 11:54:26 +0500

    Update 2023-04-13

--- drawing_sets_tab.py
+++ drawing_sets_tab.py
@@ -92,8 +92,8 @@ def drawing_sets():
         st.divider()
         st.write('Details for Drawing Set')
         st.experimental_data_editor(df.loc[df.unit == unit_selected][
-                                        'coordinator', 'performer', 'stage', 'revision', 'start_date', 'status',
-                                    'transmittal', 'trans_date', 'notes'], use_container_width=True)
+                                        ['coordinator', 'performer', 'stage', 'revision', 'start_date', 'status',
+                                    'transmittal', 'trans_date', 'notes']], use_container_width=True)
         st.divider()
 
         st.subheader(f"Project: :red[{proj_selected}]. Unit: :red[{unit_selected}]")
