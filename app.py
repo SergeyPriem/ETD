5ac1f019d12261d7bc1d03bfd43564430a59e56c
SergeyPriem <s.priemshiy@gmail.com>
2023-04-13 12:50:26 +0500

    Update 2023-04-13

--- app.py
+++ app.py
@@ -343,11 +343,7 @@ def home_content():
                                 but_key1 = f"Confirm receiving: {row.trans_num}"
                                 but_key2 = f"Close: {row.trans_num}"
 
-                                if row.received:
-                                    if st.session_state.user not in row.received:
-                                        st.button(label=but_key1, key=but_key1, type='secondary', on_click=confirm_trans,
-                                                  args=((row.trans_num,)))
-                                else:
+                                if st.session_state.user not in row.received:
                                     st.button(label=but_key1, key=but_key1, type='secondary', on_click=confirm_trans,
                                               args=((row.trans_num,)))
 
