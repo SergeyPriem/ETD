f579d1eb1c84e8405dd03c3cf8dab4907ce04ccd
SergeyPriem <s.priemshiy@gmail.com>
2023-04-13 13:21:14 +0500

    Update 2023-04-13

--- trans_close.py
+++ trans_close.py
@@ -2,16 +2,14 @@
 
 import streamlit as st
 
+
 def transmittal_close(trans_col):
     trans_col.header('Close Transmittal')
     with trans_col.form('confirm_trans'):
-        out_num = trans_col.text_input('Number of outgoing')
-        out_date = trans_col.date_input('Number of outgoing')
+        out_num = st.text_input('Number of outgoing')
+        out_date = st.date_input('Number of outgoing')
 
-        conf_but = trans_col.form_submit_button('Close')
+        conf_but = st.form_submit_button('Close')
 
         if conf_but:
             st.info(f"{out_num} by {out_date}")
-
-
-
