f38aadc5c72251a8f1e7c4fe362b03adba03de8c
SergeyPriem <s.priemshiy@gmail.com>
2023-04-17 11:58:08 +0500

    Update 2023-04-15

--- tasks_tab.py
+++ tasks_tab.py
@@ -194,7 +194,7 @@ def view_tasks(ass_tab2, my_all):
         df = df.set_index('s_o_d').join(sod_df[['project_id', 'set_name']])
         df = df.set_index('project_id').join(proj_df[['short_name']])
         df = df.set_index('speciality').join(spec_df[['abbrev']])
-        df.rename(columns={'abbrev': 'speciality', 'short_name': 'project'})
+        df.rename(columns={'abbrev': 'speciality', 'short_name': 'project'}, inplace=True)
         df.set_index('task_id', inplace=True)
 
         df = df[['project', 'set_name', 'stage', 'in_out', 'date', 'speciality', ' description', 'link',
