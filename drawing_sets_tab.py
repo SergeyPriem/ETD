45743b0b31e864740ae409059c2231f46f9c1254
SergeyPriem <s.priemshiy@gmail.com>
2023-04-17 17:24:51 +0500

    Update 2023-04-17

--- drawing_sets_tab.py
+++ drawing_sets_tab.py
@@ -8,6 +8,7 @@ from pre_sets import specialities, sod_revisions, sod_statuses
 from projects import get_trans_nums, update_sod
 from pre_sets import reporter
 
+st.write(st.session_state.edit_sod)
 
 def show_sets():
     if st.session_state.edit_sod['project']:
@@ -43,7 +44,7 @@ def show_sets():
 
     empty1, content, empty2 = st.columns([7, 30, 1])
     with empty1:
-        st.write(st.session_state.edit_sod)
+        st.empty()
     with empty2:
         st.empty()
 
