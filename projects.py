eca8ae543bde2a15f5c3994c1eaca2416e9dabe6
SergeyPriem <s.priemshiy@gmail.com>
2023-04-25 15:08:39 +0500

    Update 2023-04-25

--- projects.py
+++ projects.py
@@ -566,6 +566,9 @@ def confirm_task(task_id):
             sod = Task[task_id].s_o_d
             coord, perform = sod.coord_id, sod.perf_id
 
+            st.header(f"coord={coord}")
+            st.header(f"perform={perform}")
+
             if user == coord.login:
                 Task[task_id].coord_log = f"{(Task[task_id].coord_log).replace('None', '')}" \
                                           f"<{user}*{str(datetime.now())[:-10]}>"
