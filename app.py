8896a047b20dc0b066367d52c17b2297f3cffda5
SergeyPriem <s.priemshiy@gmail.com>
2023-04-25 18:29:23 +0500

    Update 2023-04-25

--- app.py
+++ app.py
@@ -28,7 +28,7 @@ def get_menus():
     menu = None
     icons = None
 
-    performer_menu = ["Home", "Drawing Sets", "Transmittals", "Tasks", 'colPy', 'Just for fun',
+    performer_menu = ["Home", "Drawing Sets", "Transmittals", "Tasks", 'Py', 'Just for fun',
                       'Lessons Learned', 'Settings']
 
     performer_icons = ['house', 'bi bi-file-earmark-spreadsheet-fill', 'bi bi-file-arrow-down',
@@ -668,7 +668,7 @@ def win_selector(selected):
     if selected == "Just for fun":
         just_for_fun()
 
-    if selected == "colPy":
+    if selected == "Py":
         col_py()
 
     if selected == "Manage Users":
