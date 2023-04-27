fe8b6993925f5c67f4c6e549ba841effb0983e52
SergeyPriem <s.priemshiy@gmail.com>
2023-04-27 12:35:07 +0500

    Update 2023-04-27 (script access)

--- lesson_learned_tab.py
+++ lesson_learned_tab.py
@@ -32,7 +32,8 @@ def lessons_content():
 
     with content:
         st.title(':orange[Lessons Learned] - under Development')
-        st.text('It would be great to collect, store and share experiences of technical solutions from which lessons can be learned')
+        st.write('It would be great to collect, store and share experiences of technical'
+                 ' solutions from which lessons can be learned')
 
         content_col1, content_col2 = st.columns(2, gap="medium")
         with content_col1:
