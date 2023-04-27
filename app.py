60ebd2db982d4686d05d3b1bfe0ee06938f20cd5
SergeyPriem <s.priemshiy@gmail.com>
2023-04-27 15:54:29 +0500

    Update 2023-04-27 (script access)

--- app.py
+++ app.py
@@ -727,8 +727,9 @@ def manage_users():
 
 def fresh_data():
     st.session_state.adb = get_all()
-    but_str = st.markdown("<h1 style='text-align: center; color: #00bbf9;'>Data is Fresh</h1>", unsafe_allow_html=True)
-    if st.button(but_str, key='close_fresh'):
+    st.markdown("<h1 style='text-align: center; color: #00bbf9;'>Data is Fresh</h1>", unsafe_allow_html=True)
+    lc, cc, rc  = st.columns([10, 3, 10])
+    if cc.button("O K", key='close_fresh', use_container_width=True):
         win_selector(st.session_state.selected)
 
 
