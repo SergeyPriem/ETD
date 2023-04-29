9423a3804c1a480216ec641b045d3b68441a068b
SergeyPriem <s.priemshiy@gmail.com>
2023-04-29 15:03:36 +0500

    Update 2023-04-28 (speed up Home)

--- scripts.py
+++ scripts.py
@@ -1,11 +1,12 @@
 ﻿# -*- coding: utf-8 -*-
 import datetime
 import io
-
+import ezdxf
 import streamlit as st
 import pandas as pd
 import numpy as np
 import math
+
 
 from users import err_handler
 
@@ -786,9 +787,48 @@ def making_cablist(loads_df, incom_margin, cab_df, show_settings, min_sect, cont
         loads_df = sc_rating_polarity(max_sc, loads_df, row)
 
 
+def add_gen_data(msp, loads_df, loads_df_new, point, max_sc, peak_sc):
+    ins_block = msp.add_blockref('NORM_MODE_A', insert=(0, -4689))
+    att_values = {
+        'P_RATED_A': f"Prat.sec.A={p_rat_a} kW",
+        'P_DEMAND_A': f"Pdem.sec.A={round(loads_df['rated_power'][0], 1)} kW",
+        'I_CALC_A': f"Icalc.sec.A={round(loads_df['rated_current'][0], 1)} A",
+        'COS_A': f"Cos f.sec.A={round(loads_df.power_factor[0], 2)}"
+    }
+    ins_block.add_auto_attribs(att_values)
+
+    ins_block = msp.add_blockref('EMERG_MODE', insert=(0, -4689))
+    att_values = {
+        'P_RATED_EM': f"Prat.em.={p_rat_em} kW",
+        'P_DEMAND_EM': f"Pdem.em.={round(loads_df.peak_kw_pe[0], 1)} kW",
+        'I_CALC_EM': f"Icalc.em.={round(loads_df.rated_current_pe[0], 1)} A",
+        'COS_EM': f"Cos f.em.={round(loads_df_new.power_factor_pe[0], 2)}"
+    }
+    ins_block.add_auto_attribs(att_values)
+
+    ins_block = msp.add_blockref('EMERG_MODE', insert=(point - 230, -4689))
+    ins_block.add_auto_attribs(att_values)
+
+    ins_block = msp.add_blockref('NORM_MODE_B', insert=(point, -4689))
+    att_values = {
+        'P_RATED_B': f"Prat.sec.B={p_rat_b} kW",
+        'P_DEMAND_B': f"Pdem.sec.B={loads_df.rated_power[1]} kW",
+        'I_CALC_B': f"Icalc.sec.B={loads_df.rated_current[1]} A",
+        'COS_B': f"Cos f.sec.B={loads_df.power_factor[1]}"
+    }
+    ins_block.add_auto_attribs(att_values)
+
+    ins_block = msp.add_blockref('BUS_DATA', insert=(0, -4689))
+    att_values = {
+        'BUS_DATA_RU': f'400/230В, TN-S, 50Гц, {loads_df.CB_RATING[0]}A, {max_sc}кА/1с, {peak_sc}кА',
+        'BUS_DATA_EN': f'400/230В, TN-S, 50Гц, {loads_df.CB_RATING[0]}A, {max_sc}кА/1с, {peak_sc}кА'
+    }
+    ins_block.add_auto_attribs(att_values)
+
+
 def xl_to_sld():
     COS_START = 0.4
-    K_START = 1
+    # K_START = 1
     SIN_START = math.sin(math.acos(COS_START))
 
     col_1, col_content, col_2 = st.columns([1, 9, 1])
@@ -919,5 +959,162 @@ def xl_to_sld():
                     rc.text('')
                     create_sld_but = rc.form_submit_button('Create SLD', use_container_width=True)
 
+                try:
+                    doc = ezdxf.readfile(dxf_template)
+                except IOError:
+                    st.warning(f"Not a DXF file or a generic I/O error.")
+                    st.stop()
+                except ezdxf.DXFStructureError:
+                    print(f"Invalid or corrupted DXF file.")
+                    st.stop()
 
+                msp = doc.modelspace()
+                point = 0
+
+                # .astype(str).str.replace('710-', '', regex=True)
+
+                loads_df['CONSUM-CABLE_TAG'] = loads_df['CONSUM-CABLE_TAG'].astype(str).str.replace('710-', '',
+                                                                                                    regex=True)
+                loads_df['CONSUM-CABLE_TAG'] = loads_df['CONSUM-CABLE_TAG'].astype(str).str.replace('715-', '',
+                                                                                                    regex=True)
+                loads_df['HEATER-CABLE_TAG'] = loads_df['HEATER-CABLE_TAG'].astype(str).str.replace('710-', '',
+                                                                                                    regex=True)
+                loads_df['HEATER-CABLE_TAG'] = loads_df['HEATER-CABLE_TAG'].astype(str).str.replace('715-', '',
+                                                                                                    regex=True)
+                loads_df['LCS1-CABLE_TAG1'] = loads_df['LCS1-CABLE_TAG1'].astype(str).str.replace('710-', '',
+                                                                                                  regex=True)
+                loads_df['LCS1-CABLE_TAG1'] = loads_df['LCS1-CABLE_TAG1'].astype(str).str.replace('715-', '',
+                                                                                                  regex=True)
+                loads_df['LCS1-CABLE_TAG2'] = loads_df['LCS1-CABLE_TAG2'].astype(str).str.replace('710-', '',
+                                                                                                  regex=True)
+                loads_df['LCS1-CABLE_TAG2'] = loads_df['LCS1-CABLE_TAG2'].astype(str).str.replace('715-', '',
+                                                                                                  regex=True)
+                loads_df['LCS2-CABLE_TAG'] = loads_df['LCS2-CABLE_TAG'].astype(str).str.replace('710-', '', regex=True)
+                loads_df['LCS2-CABLE_TAG'] = loads_df['LCS2-CABLE_TAG'].astype(str).str.replace('715-', '', regex=True)
+
+                loads_df_A = loads_df.loc[
+                    (loads_df['bus'] != 'B') & (loads_df['equip'] != 'INCOMER') & (loads_df['equip'] != 'SECT_BREAKER')]
+                len_A = loads_df_A.shape[0]
+                loads_df_A.loc[:, 'CB_TAG'] = range(1, len_A + 1)
+                loads_df_A.loc[:, 'BUS_NUMBER'] = 'A'
+                # loads_df_A.loc[:, 'CB_TAG'] = str(loads_df_A.CB_TAG) + 'A'
+
+                loads_df_B = loads_df.loc[
+                    (loads_df['bus'] == 'B') & (loads_df['equip'] != 'INCOMER') & (loads_df['equip'] != 'SECT_BREAKER')]
+                # loads_df_B.loc[:, 'CB_TAG'] = str(loads_df_B.CB_TAG) + 'B'
+
+                len_B = loads_df_B.shape[0]
+                if len_B > 0:
+                    loads_df_B.loc[:, 'CB_TAG'] = range(1, len_B + 1)
+                    loads_df_B.loc[:, 'BUS_NUMBER'] = 'B'
+
+                loads_df_inc1 = loads_df.loc[(loads_df['equip'] == 'INCOMER') & (loads_df['bus'] == 'A')]
+                loads_df_inc1.loc[:, 'CB_TAG'] = 1
+                loads_df_inc1.loc[:, 'starter_type'] = 'INCOMER'
+                loads_df_inc1.loc[:, 'BUS_NUMBER'] = 'A'
+
+                loads_df_inc2 = loads_df.loc[(loads_df['equip'] == 'INCOMER') & (loads_df['bus'] == 'B')]
+                loads_df_inc2.loc[:, 'CB_TAG'] = 1
+                loads_df_inc2.loc[:, 'starter_type'] = 'INCOMER'
+                loads_df_inc2.loc[:, 'BUS_NUMBER'] = 'B'
+
+                loads_df_sb = pd.DataFrame()
+
+                if loads_df.loc[(loads_df['equip'] == 'SECT_BREAKER')].shape[0] == 1:
+                    loads_df_sb = loads_df.loc[(loads_df['equip'] == 'SECT_BREAKER')]
+                    loads_df_sb.loc[:, 'CB_TAG'] = 1000
+                    loads_df_sb.loc[:, 'BUS_NUMBER'] = 'A/B'
+                    loads_df_sb.loc[:, 'starter_type'] = 'SECT_BREAKER'
+
+                if order == "ПО ТИПУ ФИДЕРОВ":
+                    loads_df_A = loads_df_A.sort_values(by='starter_type', ascending=False)
+                    loads_df_A.loc[:, 'CB_TAG'] = range(2, len_A + 2)
+
+                    loads_df_B = loads_df_B.sort_values(by='starter_type', ascending=False)
+                    loads_df_B.loc[:, 'CB_TAG'] = range(2, len_B + 2)
+
+                loads_df_new = pd.concat([loads_df_inc1, loads_df_A, loads_df_sb, loads_df_B, loads_df_inc2])
+
+                loads_df_new.loc[:, 'app_num'] = loads_df_new.CB_TAG.astype('str') + loads_df_new.BUS_NUMBER
+
+                for i in range(loads_df_new.shape[0]):
+                    ins_block = msp.add_blockref(loads_df_new.starter_type[i], insert=(point, -5000))
+
+                    if loads_df_new.CB_TAG[i] < 10:
+                        feeder_num = '0' + str(loads_df_new.CB_TAG[i])
+                    else:
+                        feeder_num = str(loads_df_new.CB_TAG[i])
+
+                    if loads_df_new.CB_TAG[i] == 1000:
+                        feeder_num = ''
+
+                    bus_num = loads_df_new.BUS_NUMBER[i]
+
+                    att_values = {
+                        'CB_RATING': str(loads_df_new.CB_RATING[i]) + 'A',
+                        'CB_NUM': str(bus_num) + str(feeder_num),
+                        'CB_AMPACITY': str(loads_df_new.CB_AMPACITY[i]) + 'A',
+                        'CB_TRIP-UNIT': loads_df_new.CB_SET[i],
+                        'CB_SC-RATING_POLARITY': loads_df_new.RAT_POL[i],
+                        'CONSUM-CABLE_TAG': loads_df_new['CONSUM-CABLE_TAG'][i],
+                        'CONSUM-CABLE_TYPE': loads_df_new['CONSUM-CABLE_TYPE'][i].replace('.0m', 'm').replace('.0/',
+                                                                                                              '/'),
+                        'CONSUMER_TAG': loads_df_new.index[i],
+                        'CONSUMER_POWER': round(loads_df_new.rated_power[i], 1),
+                        'CONSUMER_AMPACITY': round(loads_df_new.rated_current[i] * loads_df_new.eff[i], 1),
+                        'CONSUMER_COS': round(loads_df_new.power_factor[i], 2),
+                        'CONSUMER_EFFICIENCY': round(loads_df_new.eff[i], 2),
+                        'CONSUMER_OPER-MODE': loads_df_new.load_duty[i],
+                        'SCHEME_TYPE': loads_df_new.SCHEME_TYPE[i],
+                        'CONT_NUM': 'C' + str(feeder_num) + str(bus_num),
+                        'CONT_AMPACITY': loads_df_new.CONT_AMPACITY[i],
+                        'VFD_AMPACITY': loads_df_new.VFD_AMPACITY[i],
+                        'VFD_TAG': loads_df_new.VFD_TAG[i],
+                        'HEATER-CABLE_TAG': loads_df_new['HEATER-CABLE_TAG'][i].replace('.0m', 'm'),
+                        'HEATER-CABLE_TYPE': loads_df_new['HEATER-CABLE_TYPE'][i].replace('.0m', 'm').replace('.0/',
+                                                                                                              '/'),
+                        'LCS1-CABLE_TAG1': loads_df_new['LCS1-CABLE_TAG1'][i].replace('.0m', 'm'),
+                        'LCS1-CABLE_TYPE1': loads_df_new['LCS1-CABLE_TYPE1'][i].replace('.0m', 'm'),
+                        'LCS1-CABLE_TAG2': loads_df_new['LCS1-CABLE_TAG2'][i].replace('.0m', 'm'),
+                        'LCS1-CABLE_TYPE2': loads_df_new['LCS1-CABLE_TYPE2'][i].replace('.m0', 'm'),
+                        'LCS2-CABLE_TAG': loads_df_new['LCS2-CABLE_TAG'][i].replace('.0m', 'm'),
+                        'LCS2-CABLE_TYPE': loads_df_new['LCS2-CABLE_TYPE'][i].replace('.0m', 'm'),
+                        # 'LCS3-CABLE_TAG':  loads_df_new['LCS3-CABLE_TAG'][i],
+                        # 'LCS3-CABLE_TYPE':  loads_df_new['LCS3-CABLE_TYPE'][i],
+                        'CONSUMER_DESCR': loads_df_new['load_service'][i],
+                        # msp.add_mtext(loads_df_new['load_service'][i], dxfattribs={"style": "OpenSans"})
+                        'MCU_TAG': 'MCU' + str(feeder_num) + str(bus_num)
+                    }
+
+                    ins_block.add_auto_attribs(att_values)
+
+                    step = 73.25 if loads_df_new.starter_type[i] != 'CB' else 50.25
+                    step = 73.25 if loads_df_new.starter_type[i] != 'CB' else 50.25
+
+                    if loads_df_new.equip[i] == "INCOMER":
+                        step = 52.215
+
+                    if loads_df_new.equip[i] == "SECT_BREAKER":
+                        step = 58.472
+
+                    point += step
+
+
+                add_gen_data(msp, loads_df, loads_df_new, point)
+
+                dxf_file = doc.saveas('SLD.dxf')
+
+                report = ("ОДНОЛИНЕЙКА В ФОРМАТЕ .dxf ГОТОВА. Качайте файл SLD.dxf")
+
+                st.success(report)
+
+
+                buffer_2 = io.BytesIO()
+
+                buffer_2.write(dxf_file)
+
+                st.download_button('Get SLD', data=buffer_2,
+                                   file_name=f'SLD {datetime.datetime.today().strftime("%Y-%m-%d-%H-%M")}.xlsx',
+                                   mime=None, key=None, help=None, on_click=None, args=None, kwargs=None,
+                                   disabled=False, use_container_width=False)
 
