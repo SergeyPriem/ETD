f8b1dc1ffc6b626cb04ab52c6f17b42cf59c8762
SergeyPriem <s.priemshiy@gmail.com>
2023-04-29 14:12:58 +0500

    Update 2023-04-28 (speed up Home)

--- scripts.py
+++ scripts.py
@@ -915,7 +915,7 @@ def xl_to_sld():
             with tab_sld:
                 with st.form('create_sld'):
                     lc, rc = st.columns(2, gap='medium')
-                    order = lc.radio("Order od SLD creation", ('ПО ТИПУ ФИДЕРОВ', 'ПО LOAD LIST'))
+                    order = lc.radio("Order od SLD creation", ('ПО ТИПУ ФИДЕРОВ', 'ПО LOAD LIST'), horizontal=True)
                     create_sld_but = rc.form_submit_button('Create SLD', use_container_width=True)
 
 
