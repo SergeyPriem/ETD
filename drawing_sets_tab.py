a9f4298a42de49ef46236cbcbb365263b200bb5e
SergeyPriem <s.priemshiy@gmail.com>
2023-05-01 11:11:12 +0500

    Update 2023-05-01 (temp_dxf)

--- drawing_sets_tab.py
+++ drawing_sets_tab.py
@@ -107,8 +107,8 @@ def drawing_sets():
         proj_id = proj_df[proj_df.short_name == proj_selected].index.to_numpy()[0]
         trans_list = []
         try:
-            trans_list_check = trans_df.loc[trans_df.project == proj_id, 'trans_num'].tolist()
-            st.write(type(trans_list_check))
+            trans_list = trans_df.loc[trans_df.project == proj_id, 'trans_num'].tolist()
+            st.write(trans_list)
         except Exception as e:
             st.write(err_handler(e))
             st.stop()
