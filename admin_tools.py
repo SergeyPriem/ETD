2a7a32dcfc1f865b258c76777be25519e444969b
SergeyPriem <s.priemshiy@gmail.com>
2023-05-05 13:10:55 +0500

    Update 2023-05-03 (Update informing)

--- admin_tools.py
+++ admin_tools.py
@@ -214,6 +214,8 @@ def manage_projects():
 
             proj_df = proj_df.merge(u_df.login, how='left', left_on='responsible_el', right_on='id')
 
+            proj_df.responsible_el = proj_df.login
+
             if st.button('Projects'):
                 st.experimental_data_editor(proj_df, use_container_width=True, height=1500)
 
