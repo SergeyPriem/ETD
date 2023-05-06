841f870706bea4aa9a4bff09e38edc6a294a8970
SergeyPriem <s.priemshiy@gmail.com>
2023-05-06 10:15:45 +0500

    Update 2023-05-03 (Update informing)

--- app.py
+++ app.py
@@ -100,7 +100,7 @@ def show_duration():
             td = datetime.datetime.now() - st.session_state.r_now
             delta = f"{td.total_seconds()}"
 
-            st.sidebar.markdown(f"<h3 style='text-align: center; color: #00bbf9;'>{delta[:-3]} s.</h3>",
+            st.sidebar.markdown(f"<h3 style='text-align: center; color: #00bbf9;'>{delta} s.</h3>",
                                 unsafe_allow_html=True)
 
 
