41e1c33f6b99d8d29ea507856742ca6723f34b2c
SergeyPriem <s.priemshiy@gmail.com>
2023-05-15 14:09:54 +0500

    Update 2023-05-15 (Removed loop from transm.)

--- app.py
+++ app.py
@@ -304,6 +304,7 @@ def form_for_trans():
                     st.warning("Please Confirm that Transmittal Number is not Required")
                     st.stop()
 
+
             st.session_state.trans_status['status'] = status
             st.session_state.trans_status['out_note'] = out_note
             st.session_state.trans_status['out_num'] = out_num
