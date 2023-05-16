3d57eeeca3a0803f1ebba29e24aae0a89556ee18
SergeyPriem <s.priemshiy@gmail.com>
2023-05-16 13:41:42 +0500

    Update 2023-05-16 (custom theme settings)

--- app.py
+++ app.py
@@ -1018,6 +1018,7 @@ def refresher():
 
         if server_state.db_changes['table'] == 'proj':
             st.session_state.adb['project'] = get_proj_repeat()
+            st.write(st.session_state.adb['project'])
             return "Projects Updated"
     #
         # st.session_state.db_timer = server_state.db_changes['time_marker']
