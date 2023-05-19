f3d643f91d71748156c1d94d6d77b0687a1ba6f3
SergeyPriem <s.priemshiy@gmail.com>
2023-05-19 19:04:29 +0500

    Update 2023-05-19 (prod)

--- drawing_sets_tab.py
+++ drawing_sets_tab.py
@@ -182,7 +182,7 @@ def drawing_sets():
 
                     coord_report = f"{old_coord} >> {coord}" if old_coord != coord else "no changes"
                     perf_report = f"{old_perf} >> {perf}" if old_perf != perf else "no changes"
-                    rev_report = f"{old_rev} >> {rev}" if old_rev != rev else "no changes"
+                    rev_report = f"{old_rev[0]} - {old_rev[1]} >> {rev}" if old_rev != rev else "no changes"
                     status_report = f"{old_status} >> {status}" if old_status != status else "no changes"
                     trans_report = trans_num if upd_trans_chb else "no changes"
                     notes_report = f'{old_notes} >> {notes}' if old_notes != notes else "no changes"
