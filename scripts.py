a4bafa8a9cc47596a2cd74d068228bf91d3b49cd
SergeyPriem <s.priemshiy@gmail.com>
2023-05-21 15:59:53 +0500

    Update 2023-05-21 (New Update Approach)

--- scripts.py
+++ scripts.py
@@ -862,7 +862,7 @@ def scripts_tab():
             st.write('Request for Access by sergey.priemshiy@uzliti-en.com or telegram +998 90 959 80 30')
             st.stop()
         else:
-            st.title(":orange[Let's speed up the Design...]")
+            st.title(":orange[Let's speed up the Design 🚀]")
             st.write('Select the required Script')
         with st.expander("CREARE CABLE LIST AND SLD FROM LOAD LIST"):
             st.title(':orange[Create Cable List & SLD from Load List]')
