46f9a55b66791042170cbb1f33bbf53a2e2678f2
SergeyPriem <s.priemshiy@gmail.com>
2023-05-21 16:14:41 +0500

    Update 2023-05-21 (New Update Approach)

--- app.py
+++ app.py
@@ -1094,10 +1094,11 @@ def update_tables():
                 st.session_state.adb[table] = reply[table]
                 st.session_state.refresh_status = f'Units Updated by {upd_login}'
 
-                st.sidebar.success(f"Table {table} updated by {upd_login}")
+
+                st.session_state.local_marker[table]['id'] = st.session_state.new_state[table]['id']
 
                 if st.session_state.user['access_level'] == 'dev':
-                    st.session_state.local_marker[table]['id'] = st.session_state.new_state[table]['id']
+                    st.sidebar.success(f"Table {table} updated by {upd_login}")
                     time.sleep(2)
                 st.experimental_rerun()
             else:
