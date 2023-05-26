db87c13d95f09e8662dfe484bae45579d3a6c16f
SergeyPriem <s.priemshiy@gmail.com>
2023-05-26 16:32:06 +0500

    Update 2023-05-24 (Asterix added)

--- scripts.py
+++ scripts.py
@@ -1152,7 +1152,7 @@ def scripts_tab():
             st.write("Please find required templates in folder below  👇 ")
             st.code(r'\\uz-fs\Uzle\Work\Отдел ЭЛ\01 Малая Автоматизация\Шаблоны\CABLEWAYS')
             st.write("")
-            p_l, p_r = st.columns(2, gap='medium')
+            p_l, p_c, p_r = st.columns(2, gap='medium')
 
             cable_list = p_l.file_uploader("CABLE LIST", type=['xlsx'],
                                           accept_multiple_files=False, key=None,
@@ -1160,7 +1160,7 @@ def scripts_tab():
                                           kwargs=None, disabled=False, label_visibility="visible")
 
 
-            sect_template = p_r.file_uploader("SECTIONS TEMPLATE", type=['dxf'],
+            sect_template = p_c.file_uploader("SECTIONS TEMPLATE", type=['dxf'],
                                              accept_multiple_files=False, key=None,
                                              help=None, on_change=None, args=None,
                                              kwargs=None, disabled=False, label_visibility="visible")
