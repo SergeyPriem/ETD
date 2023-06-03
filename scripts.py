a0bdb4b23974dc2c5bd3f6b6c049aba7ca1fa833
SergeyPriem <s.priemshiy@gmail.com>
2023-06-03 10:48:13 +0500

    Update 2023-05-30 (Cosmetics)

--- scripts.py
+++ scripts.py
@@ -340,7 +340,8 @@ def check_loads(loads_df):
     dup_df = checkLoads_df.loc[checkLoads_df.duplicated(subset=['load_tag'], keep=False), 'load_tag']
     if len(dup_df):
         st.write('<h4 style="color:red;">Duplicates in Load Tags!!!</h4>', unsafe_allow_html=True)
-        st.write(dup_df.set_index('load_tag'))
+        dup_df = dup_df.set_index('load_tag')
+        st.write(dup_df)
 
     st.text('')
     st.success('Loads Data are Walid')
