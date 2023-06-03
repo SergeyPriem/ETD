b0034466dc249079da2ba958f024b07607d6b5b6
SergeyPriem <s.priemshiy@gmail.com>
2023-06-03 10:50:22 +0500

    Update 2023-05-30 (Cosmetics)

--- scripts.py
+++ scripts.py
@@ -341,6 +341,7 @@ def check_loads(loads_df):
     if len(dup_df):
         st.write('<h4 style="color:red;">Duplicates in Load Tags!!!</h4>', unsafe_allow_html=True)
         st.write(dup_df)
+        # for
 
     st.text('')
     st.success('Loads Data are Walid')
