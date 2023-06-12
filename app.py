a0c7a467840aa8da8411592b3a9dbff8250c429c
SergeyPriem <s.priemshiy@gmail.com>
2023-06-12 15:43:10 +0500

    Update 2023-05-30 (Cosmetics)

--- app.py
+++ app.py
@@ -951,14 +951,14 @@ def services():
         with st.expander("VISIT LOG"):
             st.title(':orange[Visit Log]')
             v_log_df = get_table(VisitLog)
-            st.experimental_data_editor(v_log_df.sort_values(by='id', key='visit_log',
-                                                             ascending=False), use_container_width=True)
+            st.experimental_data_editor(v_log_df.sort_values(by='id', ascending=False),
+                                        key='visit_log', use_container_width=True)
 
         with st.expander("ACTIONS"):
             st.title(':orange[Actions]')
             action_df = get_table(Action)
-            st.experimental_data_editor(action_df.sort_values(by='id', key='action_log',
-                                                              ascending=False), use_container_width=True)
+            st.experimental_data_editor(action_df.sort_values(by='id', ascending=False),
+                                        key='action_log', use_container_width=True)
 
 
 
