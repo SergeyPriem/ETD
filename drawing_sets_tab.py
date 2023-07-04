2f423028738197b490b58826f1ea2e051692d85e
SergeyPriem <s.priemshiy@gmail.com>
2023-07-04 18:37:48 +0500

    Update 2023-07-04 (title-help)

--- drawing_sets_tab.py
+++ drawing_sets_tab.py
@@ -180,7 +180,8 @@ def drawing_sets():
             l_c.text_area(label="Transmittals' History", value=old_trans, height=126, disabled=True)
 
             l_c.text('')
-            request_chb = l_c.checkbox('Request for Update', disabled=check_disabled)
+            request_chb = l_c.checkbox('Request for Update', disabled=check_disabled,
+                                       help=":blue[Select to Send the Request to Unit Coordinator]")
 
             c_c.text('')
             upd_unit_but = c_c.form_submit_button(label=button_label, use_container_width=True)
