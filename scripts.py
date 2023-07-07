95268da11b6dbc30d3c6a6122aca7c2bf3d6dd47
SergeyPriem <s.priemshiy@gmail.com>
2023-07-07 08:46:14 +0500

    Update 2023-07-07 (title-help)

--- scripts.py
+++ scripts.py
@@ -899,8 +899,8 @@ def scripts_tab():
 
             dxf_template = p_r.file_uploader("SLD TEMPLATE loader", type='dxf')
 
-            tab_cl, tab_sld, tab_xml = st.tabs(['1 Create Cable List  ➡️️', '2 Create SLD in DXF  ➡️',
-                                                '3 Greate XML for ETAP'])
+            tab_cl, tab_sld, tab_xml = st.tabs(['1️⃣ Create Cable List  ⏩️️', '2️⃣ Create SLD in DXF  ⏩️',
+                                                '3️⃣ Greate XML for ETAP'])
 
             with tab_cl:
 
