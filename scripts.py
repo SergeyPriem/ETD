53a46c003d969d606a84c2b400a1c308acf3c663
SergeyPriem <s.priemshiy@gmail.com>
2023-07-07 08:50:45 +0500

    Update 2023-07-07 (title-help)

--- scripts.py
+++ scripts.py
@@ -874,8 +874,8 @@ def scripts_tab():
         with st.expander("CREARE CABLE LIST  |  SLD FROM LOAD LIST  |  XML FOR ETAP"):
             cl, cc, cr = st.columns([1, 32, 1])
             cc.title(':orange[Create Cable List  |  SLD from Load List  |  Creare XML for ETAP]')
-            cr.text("", help=":blue[Каждый из перечисленных скриптов доступен на соответствующей вкладке.]  \n"
-                             ":red[Порядок работы со скриптами должен соответствовать номерам вкладок] 1️⃣ 2️⃣ 3️⃣ \n"
+            cr.text("", help=":blue[Каждый из перечисленных скриптов доступен на соответствующей вкладке] 1️⃣ 2️⃣ 3️⃣   \n"
+                             ":red[Порядок работы со скриптами должен соответствовать номерам вкладок] \n"
                              "***"
                              "\n "
                              ":blue[Each of the listed scripts is available at the corresponding tab] 1️⃣ 2️⃣ 3️⃣  \n"
