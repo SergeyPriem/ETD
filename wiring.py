ec2c0ba7e5bb2e8b375fe48f6e6dd77c725ceaed
SergeyPriem <s.priemshiy@gmail.com>
2023-07-10 19:51:43 +0500

    Update 2023-07-10 (title-help)

--- wiring.py
+++ wiring.py
@@ -56,6 +56,7 @@ def create_cab_con():
             left_pan = lc.selectbox("Select the Left Panel", left_pan_list)
             right_pan = rc.selectbox("Select the Right Panel", right_pan_list)
 
+
             cab_tag = lc.text_input("Cable Tag")
 
             cab = st.session_state.intercon['cab_descr']
@@ -63,12 +64,16 @@ def create_cab_con():
             cab_purposes = cab['cab_purpose'].tolist()
             cab_types = cab['cab_type'].tolist()
             cab_sects = cab['cab_sect'].tolist()
+            cab_tags = cab['cab_tag'].tolist()
 
             cab_purpose = rc.selectbox("Select Cable Purpose", cab_purposes)
             cab_type = lc.selectbox("Select Cable Type", cab_types)
             cab_sect = rc.selectbox("Select Wire Section", cab_sects)
 
             if cab_tag and st.button("Create Cable Connection", use_container_width=True):
+                if cab_tag in cab_tags:
+                    st.warning("Cable with such tag already exist")
+                    st.stop()
 
                 df2 = pd.DataFrame.from_dict([
                     {
