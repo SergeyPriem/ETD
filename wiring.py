d922766187d62389eb863614f193962c79f99830
SergeyPriem <s.priemshiy@gmail.com>
2023-07-11 08:42:29 +0500

    Update 2023-07-10 (title-help)

--- wiring.py
+++ wiring.py
@@ -5,7 +5,7 @@ import pandas as pd
 
 def open_inercon_doc():
     doc_sheets = pd.read_excel(st.session_state.intercon['doc'], sheet_name=None)
-    st.write(doc_sheets)
+    st.write(doc_sheets[0])
     design_sheets = ['equip', 'panel', 'block', 'terminal', 'cable', 'wire', 'cab_descr'].sort()
 
     if doc_sheets == design_sheets:
