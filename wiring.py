26348598ff21ee8eaec4b2af1033f8e4558ed1de
SergeyPriem <s.priemshiy@gmail.com>
2023-07-13 15:16:29 +0500

    Update 2023-07-10 (title-help)

--- wiring.py
+++ wiring.py
@@ -360,7 +360,7 @@ def edit_wires():
                                                 },
                                                 hide_index=True, num_rows='dynamic', use_container_width=True)
 
-            wires_to_del, wires_to_show = upd_cable_wires_df.loc[upd_cable_wires_df.wire_to_del, 'wire_uniq'].tolist()
+            wires_to_del = upd_cable_wires_df.loc[upd_cable_wires_df.wire_to_del, 'wire_uniq'].tolist()
             wires_to_show = upd_cable_wires_df.loc[upd_cable_wires_df.wire_to_del, 'wire_num'].tolist()
 
         rc.text('')
