4160be67bc7102121e9db6d601baae154f40bcdc
SergeyPriem <s.priemshiy@gmail.com>
2023-07-14 16:12:37 +0500

    Update 2023-07-10 (title-help)

--- wiring.py
+++ wiring.py
@@ -421,13 +421,13 @@ def edit_wires():
                 #     else:
                 #         upd_cable_wires_df.loc[ind, 'wire_trouble'] = "-"
 
-                upd_cable_wires_df.wire_uniq = upd_cable_wires_df.cab_tag+":"+upd_cable_wires_df.wire_num.astype('str')
+                upd_cable_wires_df.wire_uniq = upd_cable_wires_df.cab_tag+":"+upd_cable_wires_df.wire_num.astype("int").astype('str')
 
                 upd_cable_wires_df.full_term_tag_left = upd_cable_wires_df.full_block_tag_left + ":" + \
-                                                        upd_cable_wires_df.term_num_left.astype('str')
+                                                        upd_cable_wires_df.term_num_left.astype("int").astype('str')
 
                 upd_cable_wires_df.full_term_tag_right = upd_cable_wires_df.full_block_tag_right + ":" + \
-                                                         upd_cable_wires_df.term_num_right.astype('str')
+                                                         upd_cable_wires_df.term_num_right.astype("int").astype('str')
 
                 left_duplicates = upd_cable_wires_df.full_term_tag_left.duplicated()
 
