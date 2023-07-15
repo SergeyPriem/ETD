9a48f9343c4e8ce46612efaf8a52bac8dde1c310
SergeyPriem <s.priemshiy@gmail.com>
2023-07-15 14:15:43 +0500

    Update 2023-07-10 (title-help)

--- app.py
+++ app.py
@@ -812,7 +812,7 @@ def manage_users():
                                                 default_index=0,
                                                 orientation='horizontal')
                 st.text("")
-                e_l, l_c, c_c, r_c, e_c = st.columns([1,1,3,4,1], gap='medium')
+                e_l, l_c, c_c, r_c, e_c = st.columns([1,1,2,4,1], gap='medium')
                 l_c.text('')
                 l_c.text('')
                 script_acc_chb_init = l_c.checkbox('Access to Scripts', key="acc_to_scr", value=0)
