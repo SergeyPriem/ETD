fa53d7301310db16fb22e676abb2aee89a7308e8
SergeyPriem <s.priemshiy@gmail.com>
2023-07-17 17:55:13 +0500

    Update 2023-07-17 (updated pips)

--- wiring.py
+++ wiring.py
@@ -287,7 +287,7 @@ def edit_panel():
                 rc2.text('')
                 rc2.text('')
 
-                pan_to_del = pan_edited_df[pan_edited_df.pan_to_del == "True", "full_pan_tag"].tolist()
+                pan_to_del = pan_edited_df[pan_edited_df.pan_to_del, "full_pan_tag"].tolist()
                 del_pan_button = rc2.button(f"Delete selected Panels {pan_to_del}", use_container_width=True)
 
                 if del_pan_button:
