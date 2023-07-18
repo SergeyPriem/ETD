127c82ec33f8ecb9f996240b9e78aff35cbfba61
SergeyPriem <s.priemshiy@gmail.com>
2023-07-18 15:21:15 +0500

    Update 2023-07-18 (refactored)

--- intercon/blocks.py
+++ intercon/blocks.py
@@ -67,14 +67,13 @@ def edit_block():
                         "block_to_del": st.column_config.CheckboxColumn(
                             "Delete Block",
                             width="small",
-                            default=False
+                            default=False,
                         ),
                         "full_block_tag": st.column_config.TextColumn(
                             "Full Block Tag",
-                            width="small"
+                            width="small",
                         ),
-                    }, hide_index=True, use_container_width=True, num_rows='fixed'
-                )
+                    }, hide_index=True, use_container_width=True, num_rows='fixed')
 
                 blocks_to_del = \
                     blocks_edited_df.loc[blocks_edited_df.block_to_del.astype('str') == "True", "full_block_tag"].tolist()
@@ -93,7 +92,7 @@ def edit_block():
 
                         # st.write(df)
 
-                        check_list = df[df.full_block_tag.duplicated(), "full_block_tag"]
+                        check_list = df[df.full_block_tag.duplicated(), "full_block_tag"].tolist()
                         if len(check_list):
                             st.write(f"#### :red[Dulicated Terminal Blocks {check_list}. Please fix and save]")
                             st.button("OK", key='duplicated_blocks')
