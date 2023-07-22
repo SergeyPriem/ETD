85cd6b76f59a20241c7be636318b23e7bae15d1f
SergeyPriem <s.priemshiy@gmail.com>
2023-07-22 15:30:55 +0500

    Update 2023-07-22 (toast added)

--- inter_db/equipment.py
+++ inter_db/equipment.py
@@ -14,17 +14,17 @@ def edit_equipment(df):
                 for ind, row in eq_df.iterrows():
                     st.write(row.equipment_tag)
 
-                    edit_row = Equip.get(equipment_tag=row.equipment_tag)
-                    if not edit_row:
-                        st.toast(f"#### :red[Fail, equipment {row.equipment_tag} not found]")
-                        continue
-
-                    edit_row.equipment_tag = str(row.equipment_tag),
-                    edit_row.descr = str(row.descr),
-                    edit_row.to_del = False,
-                    edit_row.notes = str(row.notes)
-
-                    st.toast(f"#### :green[Equipment: {row.equipment_tag} is updated]")
+                    # edit_row = Equip.get(equipment_tag=row.equipment_tag)
+                    # if not edit_row:
+                    #     st.toast(f"#### :red[Fail, equipment {row.equipment_tag} not found]")
+                    #     continue
+                    #
+                    # edit_row.equipment_tag = str(row.equipment_tag),
+                    # edit_row.descr = str(row.descr),
+                    # edit_row.to_del = False,
+                    # edit_row.notes = str(row.notes)
+                    #
+                    # st.toast(f"#### :green[Equipment: {row.equipment_tag} is updated]")
             except Exception as e:
                 st.toast(f"Can't update {row.equipment_tag}")
                 st.toast(f"##### {err_handler(e)}")
