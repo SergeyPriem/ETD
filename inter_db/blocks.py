20deb2ea4c10df3546a6d86874b01875fa3e5ed3
SergeyPriem <s.priemshiy@gmail.com>
2023-07-24 17:25:39 +0500

    Update 2023-07-22 (toast added)

--- inter_db/blocks.py
+++ inter_db/blocks.py
@@ -40,13 +40,13 @@ def edit_block(df):
 #     except Exception as e:
 #         st.toast(err_handler(e))
 
-def create_block():
+def create_block(panel_tag):
     pan_tag_list = list(get_panel_tags())
 
     with st.form('add_block'):
         c1, c2, c3, c4, c5 = st.columns([1, 1, 1, 1.5, 0.5], gap='medium')
         # eq_tag = c1.selectbox('Equipment Tag', eqip_tag_list)
-        panel_tag = c1.selectbox('Panel Tag', pan_tag_list)
+        # panel_tag = c1.selectbox('Panel Tag', pan_tag_list)
         block_tag = c2.text_input('Block Tag')
         block_descr = c3.text_input('Block Description')
         block_notes = c4.text_input('Notes')
@@ -113,7 +113,7 @@ def blocks_main(act, prev_dict, prev_sel):
 
     if act == 'Create':
         data_to_show
-        create_block()
+        create_block(selected_panel)
 
     if act == 'View':
         data_to_show
