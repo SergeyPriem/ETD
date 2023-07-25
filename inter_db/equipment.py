ae276e763490e1c31ea2eaf7494d9c994f05d1ee
SergeyPriem <s.priemshiy@gmail.com>
2023-07-25 08:02:30 +0500

    Update 2023-07-22 (toast added)

--- inter_db/equipment.py
+++ inter_db/equipment.py
@@ -28,7 +28,7 @@ def edit_equipment(df):
                 st.toast(f"Can't update {str(row.equipment_tag)}")
                 st.toast(f"##### {err_handler(e)}")
             except IntegrityError as e2:
-                st.toast(f"Can't update {str(row.equipment_tag)}")
+                st.toast(f"Equipment {str(row.equipment_tag)} already exists...")
                 st.toast(f"##### {err_handler(e2)}")
 
             finally:
