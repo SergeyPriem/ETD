09a4df790b5aab1dfe648da6b4fafc819bcf39bc
SergeyPriem <s.priemshiy@gmail.com>
2023-08-01 11:41:20 +0500

    Update 2023-07-31 (radio_panels)

--- inter_db/wires.py
+++ inter_db/wires.py
@@ -441,7 +441,7 @@ def wires_main(act):
 
         if act == 'Create':
             data_to_show
-            st.write(cab_df.loc[cab_df.cable_tag == cab_tag].at['wire'])
+            st.write(cab_df.at[cab_df.cable_tag == cab_tag, 'wire'])
             # create_wires(cab_tag, cab_df.loc[cab_df.cable_tag == cab_tag, 'wire'].astype('int'))
 
         if act == 'View':
