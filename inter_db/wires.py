cafde242c871c3747c0d78576765bdebe09b99aa
SergeyPriem <s.priemshiy@gmail.com>
2023-08-01 16:50:17 +0500

    Update 2023-07-31 (radio_panels)

--- inter_db/wires.py
+++ inter_db/wires.py
@@ -298,13 +298,13 @@ def get_filtered_wires(cab_tag):
 
 
 def delete_wires(cab_tag):
-    try:
-        with db_session:
-            cab = Cable.get(cable_tag=cab_tag)
-            delete(w for w in Wire if w.cab_id == cab)
-        st.toast("All wires deleted")
-    except Exception as e:
-        st.toast(err_handler(e))
+    # try:
+    with db_session:
+        cab = Cable.get(cable_tag=cab_tag)
+        delete(w for w in Wire if w.cab_id == cab)
+    st.toast("All wires deleted")
+    # except Exception as e:
+    #     st.toast(err_handler(e))
 
 
 def wires_main(act):
