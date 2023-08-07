bac250d88d5728fa07a98cd6d4160bcf77049c9c
SergeyPriem <s.priemshiy@gmail.com>
2023-08-07 08:37:25 +0500

    Update 2023-08-03 (relation wire-terminal)

--- inter_db/panels.py
+++ inter_db/panels.py
@@ -128,12 +128,13 @@ def copy_panel(selected_equip, panel_tag):
                       panel_un=str(eq_tag) + ":" + str(panel_tag))
 
                 st.toast(f"""#### :green[Panel {panel_tag}: {panel_descr} added!]""")
-
+                st.write(copy_nested_blocks)
                 if copy_nested_blocks:
                     panel = select(p for p in Panel if p.eq_id == eq_id and p.panel_tag == panel_tag).first()
+                    st.write(panel)
                     panel_blocks = select(b for b in Block if b.pan_id == panel)[:]
                     # panel_blocks_df = select(b for b in Block if )
-
+                    st.write(panel_blocks)
                     for block in panel_blocks:
                         add_block_to_db(eq_tag, panel_tag, block_tag=block.block_tag,
                                         block_descr=block.description, block_notes=block.notes)
