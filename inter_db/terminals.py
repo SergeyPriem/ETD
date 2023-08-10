4de792785f56fd7f7fe02582149c573c54562515
SergeyPriem <s.priemshiy@gmail.com>
2023-08-10 11:54:57 +0500

    Update 2023-08-09 (copy Block)

--- inter_db/terminals.py
+++ inter_db/terminals.py
@@ -89,7 +89,7 @@ def delete_terminals(df):
 def terminals_main(act):
     eq_tag_list = list(get_eqip_tags())
 
-    c1, c2, c3 = st.columns([1, 2, 1], gap='medium')
+    c1, c2, c3 = st.columns([2, 2, 1], gap='medium')
 
     if len(eq_tag_list) == 0:
         eq_tag_list = ['No equipment available']
