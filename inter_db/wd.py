43cfa89df83dca5658044dae58f9d1eda0115528
SergeyPriem <s.priemshiy@gmail.com>
2023-08-11 16:27:20 +0500

    Update 2023-08-09 (copy Block)

--- inter_db/wd.py
+++ inter_db/wd.py
@@ -11,66 +11,63 @@ from utilities import err_handler, tab_to_df, open_dxf_file, save_uploaded_file
 import ezdxf
 
 def get_all_terminals(equip_tag):
-    try:
-        with db_session:
-            equip = Equip.get(equipment_tag=equip_tag)
-            panels = select(p for p in Panel if p.eq_id == equip)[:]
-            cables = select(cab for cab in Cable if cab.left_pan_id in panels)[:]
-            wires = select(
-                (
-                    w.id,
+    # try:
+    with db_session:
+        equip = Equip.get(equipment_tag=equip_tag)
+        panels = select(p for p in Panel if p.eq_id == equip)[:]
+        cables = select(cab for cab in Cable if cab.left_pan_id in panels)[:]
+        wires = select(
+            (
+                w.id,
+                w.left_term_id.block_id.pan_id.eq_id.equipment_tag,
+                w.left_term_id.block_id.pan_id.eq_id.descr,
+                w.left_term_id.block_id.pan_id.eq_id.notes,
+                w.left_term_id.block_id.pan_id.panel_tag,
+                w.left_term_id.block_id.pan_id.descr,
+                w.left_term_id.block_id.pan_id.notes,
+                w.left_term_id.block_id.block_tag,
+                w.left_term_id.terminal_num,
+                w.left_term_id.int_circuit,
+                w.left_term_id.int_link,
+                w.left_term_id.notes,
+                w.cable_id.cable_tag,
+                w.cable_id.purpose_id.circuit_descr,
+                w.cable_id.type_id.cab_type,
+                w.cable_id.wires_id.wire_num,
+                w.cable_id.sect_id.section,
+                w.cable_id.notes,
+                w.wire_num,
+                w.right_term_id.block_id.pan_id.eq_id.equipment_tag,
+                w.right_term_id.block_id.pan_id.eq_id.descr,
+                w.right_term_id.block_id.pan_id.eq_id.notes,
+                w.right_term_id.block_id.pan_id.panel_tag,
+                w.right_term_id.block_id.pan_id.descr,
+                w.right_term_id.block_id.pan_id.notes,
+                w.right_term_id.block_id.block_tag,
+                w.right_term_id.terminal_num,
+                w.right_term_id.int_circuit,
+                w.right_term_id.int_link,
+                w.right_term_id.notes,
+                w.notes
+            )
+            for w in Wire if w.cable_id in cables)[:]
 
-                    w.left_term_id.block_id.pan_id.eq_id.equipment_tag,
-                    w.left_term_id.block_id.pan_id.eq_id.descr,
-                    w.left_term_id.block_id.pan_id.eq_id.notes,
-                    w.left_term_id.block_id.pan_id.panel_tag,
-                    w.left_term_id.block_id.pan_id.descr,
-                    w.left_term_id.block_id.pan_id.notes,
-                    w.left_term_id.block_id.block_tag,
-                    w.left_term_id.terminal_num,
-                    w.left_term_id.int_circuit,
-                    w.left_term_id.int_link,
-                    w.left_term_id.notes,
+        wires_df = pd.DataFrame(
+            data=wires, columns=[
+                'id',
+                'left_equip_tag', 'left_equip_descr', 'left_equip_notes',
+                'left_panel_tag', 'left_panel_descr', 'left_panel_notes',
+                'left_block_tag', 'left_term', 'left_int_circ', 'left_jumper', 'left_term_note',
+                'cable_tag', 'cable_descr', 'cable_type', 'cable_wires', 'cable_section', 'cable_notes', 'wire_num',
+                'right_equip_tag', 'right_equip_descr', 'right_equip_notes',
+                'right_panel_tag', 'right_panel_descr', 'right_panel_notes',
+                'right_block_tag', 'right_term', 'right_int_circ', 'right_jumper', 'right_term_note',
+                'wire_notes',
+            ])
+        return wires_df
 
-                    w.cable_id.cable_tag,
-                    w.cable_id.purpose_id.circuit_descr,
-                    w.cable_id.type_id.cab_type,
-                    w.cable_id.wires_id.wire_num,
-                    w.cable_id.sect_id.section,
-                    w.cable_id.notes,
-                    w.wire_num,
-
-                    w.right_term_id.block_id.pan_id.eq_id.equipment_tag,
-                    w.right_term_id.block_id.pan_id.eq_id.descr,
-                    w.right_term_id.block_id.pan_id.eq_id.notes,
-                    w.right_term_id.block_id.pan_id.panel_tag,
-                    w.right_term_id.block_id.pan_id.descr,
-                    w.right_term_id.block_id.pan_id.notes,
-                    w.right_term_id.block_id.block_tag,
-                    w.right_term_id.terminal_num,
-                    w.right_term_id.int_circuit,
-                    w.right_term_id.int_link,
-                    w.right_term_id.notes,
-                    w.notes
-                )
-                for w in Wire if w.cable_id in cables)[:]
-
-            wires_df = pd.DataFrame(
-                data=wires, columns=[
-                    'id',
-                    'left_equip_tag', 'left_equip_descr', 'left_equip_notes',
-                    'left_panel_tag', 'left_panel_descr', 'left_panel_notes',
-                    'left_block_tag', 'left_term', 'left_int_circ', 'left_jumper', 'left_term_note',
-                    'cable_tag', 'cable_descr', 'cable_type', 'cable_wires', 'cable_section', 'cable_notes', 'wire_num',
-                    'right_equip_tag', 'right_equip_descr', 'right_equip_notes',
-                    'right_panel_tag', 'right_panel_descr', 'right_panel_notes',
-                    'right_block_tag', 'right_term', 'right_int_circ', 'right_jumper', 'right_term_note',
-                    'wire_notes'
-                ])
-            return wires_df
-
-    except Exception as e:
-        st.toast(err_handler(e))
+    # except Exception as e:
+    #     st.toast(err_handler(e))
 
 
 def draw_pan_connection(panel, dxf_template):
